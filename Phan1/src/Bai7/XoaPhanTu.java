7ae2ca50301282efacb691a7078713505d84c18b
tranxuanmanh <tranmanh2003tvt@gmail.com>
2024-09-17 22:50:06 +0700

    Lan 2

--- Phan1/src/Bai7/XoaPhanTu.java
+++ Phan1/src/Bai7/XoaPhanTu.java
@@ -1,8 +1,8 @@
-package Bai6;
+package Bai7;
 
 import java.util.Scanner;
 
-public class ChenPhanTuVaoMang {
+public class XoaPhanTu {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
@@ -11,12 +11,10 @@ public class ChenPhanTuVaoMang {
             arr[i]=sc.nextInt();
         }
         int k=sc.nextInt();
-        int x=sc.nextInt();
-        for(int i=n;i>=k+1;i--){
-            arr[i]=arr[i-1];
+        for(int i=k;i<n;i++){
+            arr[i]=arr[i+1];
         }
-        arr[k]=x;
-        n++;
+        n--;
         for(int i=0;i<n;i++){
             System.out.print(arr[i]+" ");
         }
